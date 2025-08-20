2903c5a8206c35cbba7621fc094c0ef4c52f338a
TecnologiaFundeca <informaticafundeca@gmail.com>
2025-08-20 10:48:41 -0400

    Add files via upload
    Dropdown mediaquery

---
+++ Pagina Web Fundeca/Pagina Web Fundeca/JavaScript/nav.js
@@ -0,0 +1,9 @@
+//Animaciones de NavBar
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%"
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%"
+}
+//Fin de animaciones de NavBar
