e1fa6bba94ea76b063ddc607f29d7e80e4c84c5d
TecnologiaFundeca <informaticafundeca@gmail.com>
2025-08-26 08:27:13 -0400

    Correcion de errores

--- Pagina Web Fundeca/Pagina Web Fundeca/JavaScript/nav.js
+++
@@ -1,9 +0,0 @@
-//Animaciones de NavBar
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%"
-}
-
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%"
-}
-//Fin de animaciones de NavBar
