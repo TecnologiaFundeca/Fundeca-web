48f39b83d1a9a25372b7181dc975e8634c3d6f64
TecnologiaFundeca <informaticafundeca@gmail.com>
2025-08-27 20:39:51 -0400

    Avance

--- Pagina Web Fundeca/JavaScript/nav.js
+++ Pagina Web Fundeca/JavaScript/nav.js
@@ -7,3 +7,19 @@ function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%"
 }
 //Fin de animaciones de NavBar
+//Animaciones Quienes somos
+document.addEventListener("DOMContentLoaded", () => {
+  const animados = document.querySelectorAll(".animate-left, .animate-right");
+
+  function mostrarAnimados() {
+    animados.forEach(el => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        el.classList.add("show");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", mostrarAnimados);
+  mostrarAnimados(); // por si hay elementos visibles al cargar
+});
